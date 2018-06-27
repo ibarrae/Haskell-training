e24e5acdd2262fd6d0e77fb260cfe46989a200dc
Esteban Ibarra <eibarra@administrators-MacBook-Pro-4.local>
2018-06-27 11:36:37 -0500

    Refactored functions with composition and wholemeal programming

--- src/CreditCardValidation.hs
+++ src/CreditCardValidation.hs
@@ -1,18 +1,17 @@
-module CreditCardValidation (isValidNumber, toDigits, toDigitsRev, doubleEveryOther, sumDigits)where
-import Lib
+module CreditCardValidation where
 
 isValidNumber :: Integer -> Bool
 isValidNumber cardNumber
     | cardNumber <= 0 = False
-    | otherwise = sumDigits(doubleEveryOther(toDigitsRev cardNumber))  `mod` 10 == 0
+    | otherwise = (sumDigits . doubleEveryOther . toDigitsRev $ cardNumber) `mod` 10 == 0
 
 toDigits :: Integer -> [Integer]
 toDigits n 
     | n <= 0 = []
-    | otherwise = toDigits(n `div` 10) ++ [n `mod` 10]
-
+    | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
+    
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse(toDigits n)
+toDigitsRev = reverse . toDigits 
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
@@ -20,6 +19,4 @@ doubleEveryOther [x] = [x]
 doubleEveryOther (x:(y:z)) = x : (y * 2) : doubleEveryOther z
 
 sumDigits :: [Integer] -> Integer
-sumDigits [] = 0
-sumDigits [x] = sum(toDigits x)
-sumDigits (x:y) = sumDigits(toDigits x) + sumDigits y 
+sumDigits = sum . foldMap toDigits
